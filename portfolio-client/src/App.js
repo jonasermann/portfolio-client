c64e6cf29470634a83c8740783455f269a205c4c
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-01 19:56:21 +0200

    Add Icons

--- portfolio-client/src/App.js
+++ portfolio-client/src/App.js
@@ -13,18 +13,18 @@ function App() {
           Jonas Ermann .NET Full Stack Developer
         </p>
       </header>
-      <nav className="App-nav">
-        <div className="App-nav__icon">
-          About
-        </div>
-        <div className="App-nav__icon">
-          Projects
-        </div>
-        <div className="App-nav__icon">
-          Contact
-        </div>
-      </nav>
       <div className="App-content">
+        <nav className="App-nav">
+          <div className="App-nav__icon">
+            About
+          </div>
+          <div className="App-nav__icon">
+            Projects
+          </div>
+          <div className="App-nav__icon">
+            Contact
+          </div>
+        </nav>
         {page}
       </div>
       <footer className="App-footer">
--- portfolio-client/src/components/Home.js
+++ portfolio-client/src/components/Home.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './Home.css';
 import profile from './portfolio-profile-pic.jpg';
+import linkedin from './linkedin-icon.jpg';
+import github from './github-icon.png';
 
 const Home = () => {
 
   return (
     <div className="Home">
-      <img src={profile} alt="Taken August of 2022" height="300rem" />
-      <p className="Home__text">
-        Hi! I am 28 years old and work as a full stack .Net developer! 
-        My biggest interest is building complex algorithms to solve
-        problems of mathematical nature, but any kind of data-processing
-        is interesting to me, which is why I am a developer!
-      </p>
+      <div className="Home-content">
+        <img src={profile} alt="Taken August of 2022" height="300rem" />
+        <p className="Home__text">
+          Hi! I am 28 years old and work as a full stack .Net developer! 
+          My biggest interest is building complex algorithms to solve
+          problems of mathematical nature, but any kind of data-processing
+          is interesting to me, which is why I am a developer!
+        </p>
+      </div>
+      <div className="Home-links">
+        <div className="Home__link">
+          <img src={linkedin} alt="linked in logo" height="50rem" width="50rem" />
+          <a href="https://www.linkedin.com/in/jonas-ermann-359797a2/">Linked In</a>
+        </div>
+        <div className="Home__link">
+          <img src={github} alt="github logo" height="50rem" width="50rem" />
+          <a href="https://github.com/jonasermann">Github</a>
+        </div>
+      </div>
     </div>
   )
 }
