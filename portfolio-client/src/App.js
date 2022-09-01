3f8a87b431e8212ea92979b7eee40e3959979587
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-01 15:03:21 +0200

    Add Header Content

--- portfolio-client/src/App.js
+++ portfolio-client/src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p className="App-header__text">
+          Jonas Ermann .NET Full Stack Developer
+        </p>
       </header>
     </div>
   );
