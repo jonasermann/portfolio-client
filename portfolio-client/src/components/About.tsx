d783219dc21e4d263b40fcdb6ac95d9dd08eb3d1
Jonas Ermann <jonas.ermann@hotmail.com>
2022-09-19 11:20:39 +0200

    Add About Page

---
+++ portfolio-client/src/components/About.tsx
@@ -0,0 +1,13 @@
+import './About.css';
+
+const About = () => {
+
+    return (
+        <div className="About">
+            <div className="About-content">
+            </div>
+        </div>
+    )
+}
+
+export default About;
